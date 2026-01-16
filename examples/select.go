c07ac3cb92683da93d4899c393cb3ae6dfac5f24
Brenn0219 <brennoaugusto2109@gmail.com>
2026-01-16 14:32:39 -0300

    select

---
+++ examples/select.go
@@ -0,0 +1,30 @@
+package examples
+
+import (
+	"fmt"
+	"time"
+)
+
+func Select() {
+	c1 := make(chan string, 1)
+	c2 := make(chan string, 1)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		c2 <- "two"
+	}()
+
+	for range 2 {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
