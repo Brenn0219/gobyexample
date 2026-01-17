1083774554061a94b61439ae74ef1e20c2d0616c
brenn0219 <brennoaugusto2109@gmail.com>
2026-01-17 14:59:26 -0300

    range over channels

---
+++ examples/range-over-channels.go
@@ -0,0 +1,14 @@
+package examples
+
+import "fmt"
+
+func RangeOverChannels() {
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
+
+	for e := range queue {
+		fmt.Println(e)
+	}
+}
