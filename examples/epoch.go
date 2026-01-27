eaeded4a7855bc9068425896f1def18079a23085
brenn0219 <brennoaugusto2109@gmail.com>
2026-01-27 13:20:45 -0300

    epoch

---
+++ examples/epoch.go
@@ -0,0 +1,18 @@
+package examples
+
+import (
+	"fmt"
+	"time"
+)
+
+func Epoch() {
+	now := time.Now()
+	fmt.Println(now)
+
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
+
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
+}
