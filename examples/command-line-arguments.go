894b1755bf1602579d2b1e29262d6627a3707492
brenn0219 <brennoaugusto2109@gmail.com>
2026-02-25 21:28:35 -0300

    command line arguments

---
+++ examples/command-line-arguments.go
@@ -0,0 +1,16 @@
+package examples
+
+import (
+	"fmt"
+	"os"
+)
+
+func CommandLineArguments() {
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+	arg := os.Args[3]
+
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+}
